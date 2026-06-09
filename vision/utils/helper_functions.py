8605a54900d39333aaed39d21dfa59c31ebddf3a
Angela Mithi <angelamithi@gmail.com>
2026-06-09 14:02:13 +0300

    ammended helper function

--- vision/utils/helper_functions.py
+++ vision/utils/helper_functions.py
@@ -396,3 +396,28 @@ def pred_and_store(paths: List[pathlib.Path],
 
     # 15. Return list of prediction dictionaries
     return pred_list
+
+
+def predict_single_image(img) -> Tuple[Dict, float]:
+    """Transforms and performs a prediction on a single img and returns prediction and time taken.
+    """
+    # Start the timer
+    start_time = timer()
+
+    # Transform the target image and add a batch dimension
+    img = effnetb2_transforms(img).unsqueeze(0)
+
+    # Put model into evaluation mode and turn on inference mode
+    effnetb2.eval()
+    with torch.inference_mode():
+        # Pass the transformed image through the model and turn the prediction logits into prediction probabilities
+        pred_probs = torch.softmax(effnetb2(img), dim=1)
+
+    # Create a prediction label and prediction probability dictionary for each prediction class (this is the required format for Gradio's output parameter)
+    pred_labels_and_probs = {class_names[i]: float(pred_probs[0][i]) for i in range(len(class_names))}
+
+    # Calculate the prediction time
+    pred_time = round(timer() - start_time, 5)
+
+    # Return the prediction dictionary and prediction time
+    return pred_labels_and_probs, pred_time
